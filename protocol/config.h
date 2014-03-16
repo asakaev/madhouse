c03919d472cdd009fea4bab60619e4784beebed2
Mikhail Pitertsev <platiferro@gmail.com>
2014-03-16 11:02:30 +0400

    lib event socket Server

--- protocol/config.h
+++ protocol/config.h
@@ -6,12 +6,15 @@
 //  Copyright (c) 2014 imit. All rights reserved.
 //
 
+
 #ifndef imit_sw6_config_h
 #define imit_sw6_config_h
 
 #define PACKET_LENGTH		30
 #define PACKET_DATA_FROM	10
 #define PACKET_DATA_LENGTH	15
+
+#define LOCALADDRESS 0xFF000001
 
 
 typedef void *(const packet * in, packet * out) process_func;
