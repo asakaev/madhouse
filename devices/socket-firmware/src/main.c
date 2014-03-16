6e58089ccb2286b394c8c3e64206e47988c033ed
malina <malina@MALINA>
2014-03-16 15:53:23 +0400

    реле работают

--- devices/socket-firmware/src/main.c
+++ devices/socket-firmware/src/main.c
@@ -24,24 +24,47 @@
  * Include header files for all drivers that have been imported from
  * Atmel Software Framework (ASF).
  */
+  #define F_CPU 8000000UL  // 8 MHz
+
 #include <asf.h>
 #include <util/delay.h>
 #include "bwl_uart_lib.h"
+
+#define ON_RED 0b00000001
+#define OFF 0b00000111
+
+
+
 
 int main (void)
 {
 
 	board_init();
-	DDRC=0b11111111;
-	PORTC=0b00010000;
-	
-	uart_init_withdivider(207);
-	//uart_init_withbaud(16000, 4800);
+	DDRB = 0b00000111;
+	DDRD = 0b11000000;
+	//uart_init_withdivider(207);
+	PORTB = 0b00011110;
 	while(1)
 	{
-		PORTC ^= 16;
+		
+		//if (!(PORTB & 8))
+		//{
+			//PORTB &= 0xfe;
+		//}
+		//
+		PORTD ^=128;
 		_delay_ms(1000);
-		uart_send('j');
+		PORTD ^=64;
+		////if (uart_peek() == 'n')
+		//{
+			//PORTB = 0b00000000;
+		//}
+		////if (uart_peek() == 'f')
+		//{
+			//PORTB = OFF;
+		//}
+		_delay_ms(4000);
+		////uart_send('p');
 	}
 
 }
